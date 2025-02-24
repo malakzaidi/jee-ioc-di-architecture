3d41de0835decf3b556d3ea61c908478d3b54bd3
Malak Zaidi <malakzaidi815@gmail.com>
2025-02-24 15:20:12 +0000

    resolve the exception of Null pointer in the class Presentation

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -1,10 +1,13 @@
 package pres;
 
+import dao.DaoImpl;
 import metier.MetierImpl;
 
 public class PresentationV1 {
     public static void main(String[] args) {
+        DaoImpl dao = new DaoImpl();
         MetierImpl m = new MetierImpl();
+        m.setDao(dao);
         System.out.println(m.calcul());
     }
 }
