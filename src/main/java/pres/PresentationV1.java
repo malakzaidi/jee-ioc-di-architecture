474b4eca073ef21de52a3184c4e650cdbdf3cf93
Malak Zaidi <malakzaidi815@gmail.com>
2025-02-24 15:27:35 +0000

    modify the Presentation code to inject by the constructor with paramater

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -6,7 +6,7 @@ import metier.MetierImpl;
 public class PresentationV1 {
     public static void main(String[] args) {
         DaoImpl dao = new DaoImpl();
-        MetierImpl m = new MetierImpl();
+        MetierImpl m = new MetierImpl(dao);
         m.setDao(dao);//using the setter to do the injection
         System.out.println(m.calcul());
     }
