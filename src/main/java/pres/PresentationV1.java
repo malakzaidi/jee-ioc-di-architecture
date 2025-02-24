89e588f71339098bf4f8498b7fc8f83eec9f023e
Malak Zaidi <malakzaidi815@gmail.com>
2025-02-24 15:44:40 +0000

    adjust the Presentation code to use tthe web service version of Dao Implementation

--- src/main/java/pres/PresentationV1.java
+++ src/main/java/pres/PresentationV1.java
@@ -1,11 +1,12 @@
 package pres;
 
 import dao.DaoImpl;
+import extension.DaoImplV2;
 import metier.MetierImpl;
 
 public class PresentationV1 {
     public static void main(String[] args) {
-        DaoImpl dao = new DaoImpl();
+        DaoImplV2 dao = new DaoImplV2();
         MetierImpl m = new MetierImpl(dao);//Injection des dépenedances par le constructeur
         //m.setDao(dao);//Utilisation du setter pour effectuer l'injection
         System.out.println("res"+m.calcul());
