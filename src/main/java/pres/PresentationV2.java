e3367b124381e3af93ea30ee88093b2e72ed6ed1
Malak Zaidi <malakzaidi815@gmail.com>
2025-02-24 16:08:39 +0000

    DownCasting the variable of type Object IDao

--- src/main/java/pres/PresentationV2.java
+++ src/main/java/pres/PresentationV2.java
@@ -2,6 +2,7 @@ package pres;
 
 import java.io.File;
 import java.util.Scanner;
+import dao.IDao;
 
 public class PresentationV2 {
     public static  void main(String[] args)  {
@@ -9,7 +10,7 @@ public class PresentationV2 {
             Scanner scanner = new Scanner (new File("config.txt"));
             String daoClassname = scanner.nextLine();
             Class cDao = Class.forName(daoClassname);
-            Object o =  cDao.getConstructor().newInstance();
+            IDao dao = (IDao) cDao.getConstructor().newInstance();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
